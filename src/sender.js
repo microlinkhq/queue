366fe03a8b3c338d5a0b427400086d3c8b956e92
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-03-19 10:05:50 +0100

    refactor: use micri

--- src/sender.js
+++ src/sender.js
@@ -1,19 +1,23 @@
 'use strict'
 
-const dispatch = require('micro-route/dispatch')
-const { parse: parseUrl } = require('url')
-const { send } = require('micro')
 const queue = require('./queue')
 
-module.exports = dispatch()
-  .dispatch('/', ['GET'], async (req, res) => {
-    const { query } = parseUrl(req.url, true)
+const { send, Router } = require('micri')
+const toQuery = require('to-query')()
+
+const { router, on, otherwise } = Router
+
+const isStatic = req => req.url.startsWith('/favicon.ico') || req.url.startsWith('/robots.txt')
+
+module.exports = router(
+  on.get(isStatic, (req, res) => send(res, 204, null)),
+  otherwise(async (req, res) => {
+    const query = toQuery(req)
     if (!query.url) return send(res, 200)
 
     const { priority, delay } = query
     const { id } = await queue.add(query, { priority, delay })
+
     return send(res, 201, { id })
   })
-  .dispatch('/robots.txt', 'GET', (req, res) => send(res, 204, null))
-  .dispatch('/favicon.ico', 'GET', (req, res) => send(res, 204, null))
-  .otherwise((req, res) => send(res, 405))
+)
