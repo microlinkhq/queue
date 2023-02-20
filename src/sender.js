06c5c9e080590a3ba76764b4b3b3a21e4e58e046
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-02-20 22:25:36 +0100

    refactor: remove unnecessary dependencies

--- src/sender.js
+++ src/sender.js
@@ -1,30 +1,23 @@
 'use strict'
 
 const debug = require('debug-logfmt')('queue:sender')
-const { serve, send, Router } = require('micri')
+const { createServer } = require('http')
 const toQuery = require('to-query')()
+const send = require('send-http')
 
 const { PORT } = require('./constants')
 const queue = require('./queue')
 
-const { router, on, otherwise } = Router
-
 const isStatic = req => req.url.startsWith('/favicon.ico') || req.url.startsWith('/robots.txt')
 
-const route = router(
-  on.get(isStatic, (req, res) => send(res, 204, null)),
-  otherwise(async (req, res) => {
-    const query = toQuery(req.url)
-    if (!query.url) return send(res, 200)
-
-    const { priority, delay } = query
-    const { id } = await queue.add(query, { priority, delay })
-
-    return send(res, 201, { id })
-  })
-)
-
-const server = serve(route)
+const server = createServer(async (req, res) => {
+  if (isStatic(req)) return send(res, 204)
+  const query = toQuery(req.url)
+  if (!query.url) return send(res, 200)
+  const { priority, delay } = query
+  const { id } = await queue.add(query, { priority, delay })
+  return send(res, 201, { id })
+})
 
 server.listen(PORT, () => {
   debug(`Listening at http://localhost:${PORT}`)
